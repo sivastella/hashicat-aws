58d50aa0c18149eb42eef1facfa27686a628cdda
Siva Tella <sivatella@outlook.com>
2023-04-12 21:23:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
+module "s3-bucket" {
   source  = "app.terraform.io/siva-org/s3-bucket/aws"
   version = "2.8.0"
-  bucket        = "my-s3-bucket"
-  acl           = "private"
-  bucket_prefix = {var.prefix}
-  versioning = {
-    enabled = true
-  }
-
+  bucket_prefix = var.prefix
 }
